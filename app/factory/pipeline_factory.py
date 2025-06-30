1f4b6b09c9aadea3865a46fd7cac2b1900601008
abrougui <bahaeddine.abrougui@lidl.com>
2025-06-30 17:06:44 +0200

    Add thread safety for pipelineFactory

--- app/factory/pipeline_factory.py
+++ app/factory/pipeline_factory.py
@@ -1,14 +1,18 @@
 import logging
+import threading
 
 from app.pipeline import AltTextPipeline
 
 
 class PipelineFactory:
     _pipeline = None
+    _lock = threading.Lock()
 
     @classmethod
     def get(cls):
         if cls._pipeline is None:
-            logging.info("✅ Initializing pipeline and loading models")
-            cls._pipeline = AltTextPipeline()
+            with cls._lock:
+                if cls._pipeline is None:
+                    logging.info("✅ Initializing pipeline and loading models")
+                    cls._pipeline = AltTextPipeline()
         return cls._pipeline
