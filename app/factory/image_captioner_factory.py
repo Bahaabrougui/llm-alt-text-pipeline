ed271209f673f8d37d17cbb75829abb1e381fef5
abrougui <bahaeddine.abrougui@lidl.com>
2025-07-03 18:13:16 +0200

    Add model factory

---
+++ app/factory/image_captioner_factory.py
@@ -0,0 +1,23 @@
+import threading
+
+from app.captioning import ImageCaptioner
+from app.utils.utils import log_info_message
+
+
+class ImageCaptionerFactory:
+    _image_captioner = None
+    _lock = threading.Lock()
+
+    @classmethod
+    def get(cls):
+        if cls._image_captioner is None:
+            with cls._lock:
+                if cls._image_captioner is None:
+                    log_info_message(
+                        "✅ Initializing image captioning model"
+                    )
+                    cls._image_captioner = ImageCaptioner()
+        log_info_message(
+            "✅ Fetching already initialised image captioning model"
+        )
+        return cls._image_captioner
