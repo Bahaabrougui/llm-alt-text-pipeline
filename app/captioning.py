de13f127cc63c97dcc5e66a1094832266f13b912
abrougui <bahaeddine.abrougui@lidl.com>
2025-07-04 17:25:14 +0200

    Use blip-base

--- app/captioning.py
+++ app/captioning.py
@@ -3,27 +3,27 @@ import time
 import requests
 import torch
 from PIL import Image
-from transformers import Blip2Processor, Blip2ForConditionalGeneration
+from transformers import BlipProcessor, BlipForConditionalGeneration
 
 from app.utils.utils import log_metrics, log_info_message
 
-CACHE_DIR = "/home/site/models_cache/blip2"
+CACHE_DIR = "/home/site/models_cache/blip-base"
 
 
 class ImageCaptioner:
-    def __init__(self, model_name="Salesforce/blip2-opt-2.7b"):
+    def __init__(self, model_name="Salesforce/blip-image-captioning-base"):
         log_info_message(f"✅ Initializing model: {model_name}")
+        self.model_name = model_name
         # Init processor
-        self.processor = Blip2Processor.from_pretrained(
+        self.processor = BlipProcessor.from_pretrained(
             model_name,
             use_fast=True,
             cache_dir=CACHE_DIR
         )
         # Load weights
-        self.model = Blip2ForConditionalGeneration.from_pretrained(
+        self.model = BlipForConditionalGeneration.from_pretrained(
             model_name,
             torch_dtype=torch.float32,
-            low_cpu_mem_usage=True,
             cache_dir=CACHE_DIR
         )
 
@@ -31,7 +31,7 @@ class ImageCaptioner:
             self,
             image_path: str,
             is_url: bool = False,
-            max_tokens=30,
+            max_tokens=100,
     ) -> str:
         _start_ = time.time()
         if is_url:
@@ -40,14 +40,13 @@ class ImageCaptioner:
             ).convert("RGB")
         else:
             image = Image.open(image_path).convert('RGB')
-        prompt = "Describe the image for use as alt text in 1 sentence."
+        prompt = None
         inputs = self.processor(
             images=image,
             text=prompt,
             return_tensors="pt"
         )
-        # Debug
-        log_info_message(f"BLIP2 Inputs: {list(inputs.keys())}")
+
         # Move inputs to model device (Azure functions is CPU-only)
         # inputs = {k: v.to("cpu") for k, v in inputs.items()}
 
@@ -56,30 +55,20 @@ class ImageCaptioner:
                 **inputs,
                 max_new_tokens=max_tokens,
             )
+            # Decode caption
             caption = self.processor.decode(
                 output[0],
                 skip_special_tokens=True,
             ).strip()
-            log_info_message(f"Output tokens: {output}")
-            log_info_message(f"Decoded: {caption}")
-
-        if caption == prompt:
-            raise ValueError("Prompt mirrored as caption..")
 
         # Tokens count
-        input_tokens = self.processor.tokenizer(
-            prompt, return_tensors="pt"
-        ).input_ids.shape[-1]
-        output_tokens = self.processor.tokenizer(
-            self.processor.decode(output[0], skip_special_tokens=True),
-            return_tensors="pt"
-        ).input_ids.shape[-1]
-
+        input_tokens = 0 if prompt is None else inputs["input_ids"].shape[1]
+        output_tokens = output[0].shape[-1]
         # Cost estimate: tokens × $0.0001
         cost = (input_tokens or 0 + output_tokens) * 1e-4
 
         log_metrics(
-            "Blip2",
+            self.model_name,
             input_tokens=input_tokens,
             output_tokens=output_tokens,
             cost_usd=cost,
