51ca9d6aef59ad9cf02e379d054e7721cf03e6f3
abrougui <bahaeddine.abrougui@lidl.com>
2025-07-04 18:03:16 +0200

    Update sql statement

--- local_db/init.sql
+++ local_db/init.sql
@@ -5,9 +5,9 @@ CREATE TABLE product_images (
     alt_en TEXT,
     alt_fr TEXT,
     alt_de TEXT,
-    safety_en TEXT,
-    safety_fr TEXT,
-    safety_de TEXT,
+    safety_en JSONB,
+    safety_fr JSONB,
+    safety_de JSONB,
     status TEXT DEFAULT 'pending',
     last_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
